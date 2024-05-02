205fdf065de606032b77f767f4d4cc01c517cb3a
CrisDeboi <145705469+CrisDeboi@users.noreply.github.com>
2024-05-02 20:35:16 +0100

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,45 @@
 import './Header.css'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
-        <script src="https://kit.fontawesome.com/e4c896ad96.js" crossorigin="anonymous"></script>            
-        <div class="header-box">
-            
+            <script src="https://kit.fontawesome.com/e4c896ad96.js" crossOrigin="anonymous"></script>
+            <div className="header-box">
 
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"></link>
-            <div class="header-logo">
-                <img src='logoW2.png' alt="logo"></img>
+
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"></link>
+                <div className="header-logo">
+                    <img src='logoW2.png' alt="logo"></img>
+                </div>
+                <div className="header-menu">
+                    <ul>
+                        <li><a href="/home">Inicio</a></li>
+                        <li><a href="/catalog">Catalogo</a></li>
+                        <li><a href="/assistance">Asistencia</a></li>
+                    </ul>
+                </div>
+                <div className="header-icon">
+                    <div className="header-icon-bars" onClick={() => setMenuOpen(!menuOpen)}>
+                        <FontAwesomeIcon icon={faBars} style={{ color: "#edeeff", }} />
+                    </div>
+
+                </div>
             </div>
-            <div class="header-menu">
+            <div className={`header-side-menu ${menuOpen ? 'open' : ''}`}>
                 <ul>
                     <li><a href="/home">Inicio</a></li>
-                    <li><a href="/catalog">Catalogo</a></li>
+                    <li><a href="/catalog">Catálogo</a></li>
                     <li><a href="/assistance">Asistencia</a></li>
                 </ul>
-            </div>
-            <div class="header-icons">
-                <i class="fa-sharp fa-solid fa-bars"></i>
-            </div>            
-            
 
-        </div>
+            </div>
         </>
     )
 }
